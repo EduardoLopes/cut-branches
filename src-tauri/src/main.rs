2845e0aba18b161c2ce603c136f9e13c7273f6ec
Eduardo Lopes <EduardoLopesDS@gmail.com>
2021-05-21 10:02:19 -0300

    feat: git_repo_dir command can return a serialized response

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -9,6 +9,12 @@ use std::process::Stdio;
 
 use execute::{shell, Execute};
 
+#[derive(serde::Serialize)]
+struct GitDirResponse {
+  absolute_path: String,
+  root_path: String,
+}
+
 #[tauri::command]
 fn git_repo_dir(path: String) -> String {
   let mut command = shell(format!("cd {} && git rev-parse --git-dir", path));
@@ -17,9 +23,16 @@ fn git_repo_dir(path: String) -> String {
 
   let output = command.execute_output().unwrap();
 
-  return String::from_utf8(output.stdout)
-    .unwrap()
-    .replace(".git", "");
+  let absolute_path = String::from_utf8(output.stdout).unwrap();
+
+  let root_path = absolute_path.replace(".git", "");
+
+  let response = GitDirResponse {
+    absolute_path,
+    root_path,
+  };
+
+  return serde_json::to_string(&response).unwrap();
 }
 
 fn main() {
--- src/lib/Repo/index.svelte
+++ src/lib/Repo/index.svelte
@@ -1,13 +1,23 @@
 <script lang="ts">
 	import { invoke } from '@tauri-apps/api/tauri';
 
-	export let path;
+	interface PathInfo {
+		absolute_path: string;
+		root_path: string;
+	}
 
-	invoke('git_repo_dir', { path: path }).then((real_path) => (path = real_path));
+	export let path: string;
+	let absolute_path: string;
+	let root_path: string;
+
+	invoke('git_repo_dir', { path: path }).then((real_path: string) => {
+		absolute_path = JSON.parse(real_path).absolute_path;
+		root_path = JSON.parse(real_path).root_path;
+	});
 </script>
 
 <div>
-	{path}
+	Root path: {root_path}
 </div>
 
 <style>
