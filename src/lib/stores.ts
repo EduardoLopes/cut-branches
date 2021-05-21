cda2e73d43f18a0b3260eee9c34788a90c5b75d3
Eduardo Lopes <EduardoLopesDS@gmail.com>
2021-05-21 11:43:16 -0300

    feat: repos store uses localStorage to persist data

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,3 +1,5 @@
 import { writable } from 'svelte/store';
 
-export const repos = writable<string[]>([]);
+export const repos = writable<string[]>(JSON.parse(localStorage.getItem('repos')) || []);
+
+repos.subscribe((value) => localStorage.setItem('repos', JSON.stringify(value)));
