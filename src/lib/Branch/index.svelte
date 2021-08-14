65955c22f2cddf21697a4604146e55dc967671eb
Eduardo Lopes <EduardoLopesDS@gmail.com>
2021-08-14 13:59:27 -0300

    feat: Branch is a single component

---
+++ src/lib/Branch/index.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+	import type { Branch as IBranch } from '$lib/stores';
+	import { currentRepo } from '$lib/stores';
+
+	import Delete16 from 'carbon-icons-svelte/lib/Delete16';
+	import Information16 from 'carbon-icons-svelte/lib/Information16';
+
+	export let showDeletebutton: boolean = false;
+	export let onClickDelete: (branch: Branch) => void;
+	export let onClick: (branch: Branch) => void;
+	export let branch: Branch;
+	export let selected: boolean;
+</script>
+
+<div class="branch-container" title={`${branch.current ? 'Current branch ' : ''}`}>
+	<div class="branch" class:current={branch.current} class:selected>
+		<div
+			class="name"
+			on:click={() => {
+				if (branch.current) return;
+
+				if (onClick) onClick(branch);
+			}}
+		>
+			{branch.name}
+		</div>
+
+		{#if !branch.current && showDeletebutton}
+			<div class="menu">
+				<button
+					class="delete-button"
+					on:click={() => {
+						onClickDelete(branch);
+					}}
+				>
+					<Delete16 class="delete-icon" />
+				</button>
+			</div>
+		{/if}
+	</div>
+	<div class="info">
+		{#if branch.fully_merged}
+			<div class="grid-2">
+				<span class="icon"> <Information16 /></span>
+				<div>
+					This branch is not fully merged into the current branch, {$currentRepo.current_branch}!
+				</div>
+			</div>
+		{/if}
+	</div>
+</div>
+
+<style src="./styles.scss" lang="scss"></style>
--- src/lib/Branches/index.svelte
+++ src/lib/Branches/index.svelte
@@ -1,16 +1,17 @@
 <script lang="ts">
-	import type { Branch, Repo } from '$lib/stores';
+	import type { Branch as IBranch, Repo } from '$lib/stores';
 	import { currentRepo, loadingRepoInfo } from '$lib/stores';
 
 	import Delete16 from 'carbon-icons-svelte/lib/Delete16';
 	import Information16 from 'carbon-icons-svelte/lib/Information16';
 	import DeleteModal from '$lib/DeleteModal/index.svelte';
 	import Loading from '$lib/Loading/index.svelte';
+	import Branch from '$lib/Branch/index.svelte';
 
 	import { getRepoInfo, toast } from '$lib/utils';
 	import { repos } from '$lib/stores';
 
-	let selected: Branch[] = [];
+	let selected: IBranch[] = [];
 	let showDeleteModal: boolean = false;
 
 	currentRepo.subscribe(() => {
@@ -18,7 +19,7 @@
 	});
 
 	// current branch first
-	function sort(a: Branch, b: Branch) {
+	function sort(a: IBranch, b: IBranch) {
 		if (a.current) {
 			return -1;
 		}
@@ -77,55 +78,24 @@
 	<div class="branches">
 		{#if $currentRepo.branches}
 			{#each $currentRepo.branches.sort(sort) as branch (branch.name)}
-				<div class="branch-container" title={`${branch.current ? 'Current branch ' : ''}`}>
-					<div
-						class="branch"
-						class:current={branch.current}
-						class:selected={selected.some((item) => item === branch)}
-					>
-						<div
-							class="name"
-							on:click={() => {
-								if (branch.current) return;
+				<Branch
+					{branch}
+					showDeletebutton={selected.length === 0}
+					selected={selected.some((item) => item === branch)}
+					onClick={() => {
+						if (selected.some((item) => item.name === branch.name)) {
+							selected = selected.filter((item) => item.name !== branch.name);
+							return;
+						}
 
-								if (selected.some((item) => item.name === branch.name)) {
-									selected = selected.filter((item) => item.name !== branch.name);
-									return;
-								}
-
-								selected.push(branch);
-								selected = selected;
-							}}
-						>
-							{branch.name}
-						</div>
-						{#if selected.length === 0}
-							{#if !branch.current}
-								<div class="menu">
-									<button
-										class="delete-button"
-										on:click={() => {
-											selected = [branch];
-											showDeleteModal = true;
-										}}
-									>
-										<Delete16 class="delete-icon" />
-									</button>
-								</div>
-							{/if}
-						{/if}
-					</div>
-					<div class="info">
-						{#if branch.fully_merged}
-							<div class="grid-2">
-								<span class="icon"> <Information16 /></span>
-								<div>
-									This branch is not fully merged into the current branch, {$currentRepo.current_branch}!
-								</div>
-							</div>
-						{/if}
-					</div>
-				</div>
+						selected.push(branch);
+						selected = selected;
+					}}
+					onClickDelete={() => {
+						selected = [branch];
+						showDeleteModal = true;
+					}}
+				/>
 			{/each}
 		{/if}
 		{#if selected.length > 0}
