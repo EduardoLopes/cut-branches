5a58aa9e102bfde29621eca658929c577a7816e6
Eduardo Lopes <eduardolopesds@gmail.com>
2023-03-30 12:53:17 -0300

    feat: add an icon component wrapper

---
+++ src/lib/primitives/Icon/index.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import type * as CSS from 'csstype';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLAttributes<HTMLDivElement> {
+		size?: CSS.Properties['width'];
+		color?: CSS.Properties['color'];
+	}
+
+	export let size: CSS.Properties['width'] = '1.16rem';
+	export let color: CSS.Properties['color'] = '#000';
+</script>
+
+<div class="container icon {$$restProps.class}" style="--size:{size};--color:{color};">
+	<slot />
+</div>
+
+<style lang="scss">
+	.container {
+		width: var(--size);
+		height: var(--size);
+		color: var(--color);
+	}
+</style>
