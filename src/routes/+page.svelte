9dfd51b204de65777ec6917a080313b90aad5565
Eduardo Lopes <eduardolopesds@gmail.com>
2023-04-03 20:16:08 -0300

    feat: better checks redirects on root route

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,21 +4,23 @@
 	import Branches from '$lib/Branches/index.svelte';
 	import { goto } from '$app/navigation';
 	import { repos } from '$lib/stores';
-	import { page } from '$app/stores';
+	import { navigating, page } from '$app/stores';
 
+	function checkRedirect() {
+		if ($repos.length === 0) {
+			goto('/add-first');
+		}
+
+		if ($repos.length > 0 && $page.url.pathname === '/' && !$page.params.id) {
+			goto(`/repos/${$repos[0].name}`);
+		}
+	}
 	onMount(() => {
-		let unsubscribeRepos = repos.subscribe((value) => {
-			if (value.length === 0) {
-				goto('/add-first');
-			}
-
-			if (value.length > 0 && $page.url.pathname === '/' && !$page.params.id) {
-				goto(`/repos/${value[0].name}`);
-			}
-		});
-
-		onDestroy(unsubscribeRepos);
+		checkRedirect();
 	});
+
+	// checks every route change
+	$: if ($navigating) checkRedirect();
 </script>
 
 <div class="content">
