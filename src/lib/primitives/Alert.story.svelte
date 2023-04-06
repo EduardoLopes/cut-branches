72c53e79564d46510c96b923786fbb4f0283b617
Eduardo Lopes <eduardolopesds@gmail.com>
2023-04-06 01:47:49 -0300

    feat: add Alert component

---
+++ src/lib/primitives/Alert.story.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import type { Hst } from '@histoire/plugin-svelte';
+	import Alert, { type AlertFeedback, type AlertSize } from './Alert.svelte';
+	import Flex from './Flex/index.svelte';
+	export let Hst: Hst;
+
+	let feedback: AlertFeedback = 'normal';
+	let feedbackOptions = [
+		{ value: 'danger', label: 'danger' },
+		{ value: 'info', label: 'info' },
+		{ value: 'normal', label: 'normal' },
+		{ value: 'success', label: 'success' },
+		{ value: 'warning', label: 'warning' }
+	];
+
+	let size: AlertSize = 'md';
+	let sizeOptions = [
+		{ value: 'sm', label: 'sm' },
+		{ value: 'md', label: 'md' },
+		{ value: 'lg', label: 'lg' }
+	];
+
+  let showIcon: boolean = true;
+
+</script>
+
+<Hst.Story>
+	<svelte:fragment slot="controls">
+		<Hst.Radio bind:value={feedback} bind:options={feedbackOptions} title="Feedback" />
+		<Hst.Radio bind:value={size} bind:options={sizeOptions} title="Size" />
+    <Hst.Checkbox bind:value={showIcon} title="Show Icon" />
+
+	</svelte:fragment>
+
+	<Hst.Variant title="default variants">
+    <Flex gap="1.6rem">
+      <Alert {feedback} {size} {showIcon}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et quo, eveniet corporis fugiat minus doloribus aperiam impedit magni dolor quidem iusto, cupiditate explicabo maxime aut sed consequatur dolorum quam esse! </Alert>
+      <Alert variant="secondary" {feedback} {size} {showIcon}> Lorem ipsum dolor, sit amet consectetur adipisicing elit. Et quo, eveniet corporis fugiat minus doloribus aperiam impedit magni dolor quidem iusto, cupiditate explicabo maxime aut sed consequatur dolorum quam esse! </Alert>
+    </Flex>
+	</Hst.Variant>
+</Hst.Story>
---
+++ src/lib/primitives/Alert.svelte
@@ -0,0 +1,158 @@
+<script context="module" lang="ts">
+	export type AlertVariants = 'primary' | 'secondary';
+
+	export type AlertSize = 'sm' | 'md' | 'lg';
+
+	export type AlertFeedback = 'normal' | 'danger' | 'warning' | 'success' | 'info';
+
+	export interface AlertProps {
+		variant?: AlertVariants;
+		size?: AlertSize;
+		feedback?: AlertFeedback;
+		showIcon?: boolean;
+	}
+</script>
+
+<script lang="ts">
+	import Icon from '@iconify/svelte';
+
+	type $$Props = AlertProps;
+
+	export let variant: $$Props['variant'] = 'primary';
+	export let size: $$Props['size'] = 'md';
+	export let feedback: $$Props['feedback'] = 'normal';
+	export let showIcon: boolean = true;
+</script>
+
+<div class="alert variant-{variant} size-{size} feedback-{feedback} ">
+	{#if showIcon && feedback !== 'normal'}
+		<div class="icon">
+			{#if feedback === 'danger'}
+				<Icon
+					icon="material-symbols:dangerous-rounded"
+					width="100%"
+					height="100%"
+					color="inherit"
+				/>
+			{/if}
+
+			{#if feedback === 'warning'}
+				<Icon icon="material-symbols:warning-rounded" width="100%" height="100%" color="inherit" />
+			{/if}
+
+			{#if feedback === 'success'}
+				<Icon icon="material-symbols:check-circle" width="100%" height="100%" color="inherit" />
+			{/if}
+
+			{#if feedback === 'info'}
+				<Icon icon="material-symbols:info-rounded" width="100%" height="100%" color="inherit" />
+			{/if}
+		</div>
+	{/if}
+
+	<div class="contents">
+		<slot name="title" />
+		<slot />
+	</div>
+</div>
+
+<style lang="scss">
+	.alert {
+		// font-size
+		--alert-font-size-sm: var(--font-size-sm);
+		--alert-font-size-md: var(--font-size-md);
+		--alert-font-size-lg: var(--font-size-lg);
+
+		// padding
+		--alert-padding-sm: var(--space-sm);
+		--alert-padding-md: var(--space-md);
+		--alert-padding-lg: var(--space-lg);
+
+		// icon size
+		--alert-icon-size-sm: var(--space-lg);
+		--alert-icon-size-md: var(--space-xl);
+		--alert-icon-size-lg: var(--space-xxl);
+
+		// variants
+		&.variant-primary {
+			--alert-background-color: var(--color-neutral-4);
+		}
+
+		&.variant-secondary {
+			--alert-background-color: var(--color-neutral-1) !important;
+			--alert-color: var(--alert-icon-color);
+		}
+
+		//feedback
+		&.feedback-normal {
+			--alert-background-color: var(--color-neutral-4);
+			--alert-border-style: var(--color-neutral-6);
+			--alert-color: var(--color-neutral-12);
+			--alert-border-style: var(--color-neutral-6);
+		}
+
+		&.feedback-danger {
+			--alert-background-color: var(--color-danger-4);
+			--alert-border-style: var(--color-danger-6);
+			--alert-icon-color: var(--color-danger-10);
+		}
+
+		&.feedback-warning {
+			--alert-background-color: var(--color-warning-3);
+			--alert-border-style: var(--color-warning-6);
+			--alert-icon-color: var(--color-warning-10);
+		}
+
+		&.feedback-success {
+			--alert-background-color: var(--color-success-4);
+			--alert-border-style: var(--color-success-6);
+			--alert-icon-color: var(--color-success-10);
+		}
+
+		&.feedback-info {
+			--alert-background-color: var(--color-info-4);
+			--alert-border-style: var(--color-info-6);
+			--alert-icon-color: var(--color-info-10);
+		}
+
+		// size
+		&.size-sm {
+			--alert-font-size: var(--alert-font-size-sm);
+			--alert-padding: var(--alert-padding-sm);
+			--alert-icon-size: var(--alert-icon-size-sm);
+		}
+
+		&.size-md {
+			--alert-font-size: var(--alert-font-size-md);
+			--alert-padding: var(--alert-padding-md);
+			--alert-icon-size: var(--alert-icon-size-md);
+		}
+
+		&.size-lg {
+			--alert-font-size: var(--alert-font-size-lg);
+			--alert-padding: var(--alert-padding-lg);
+			--alert-icon-size: var(--alert-icon-size-lg);
+		}
+	}
+
+	.alert {
+		display: grid;
+		grid-template-columns: auto 1fr;
+		gap: 1.6rem;
+		border-radius: 4px;
+		color: var(--alert-color);
+		font-size: var(--alert-font-size);
+		background-color: var(--alert-background-color);
+		border-color: var(--alert-border-style);
+		border-width: 1px;
+		border-style: solid;
+		padding: var(--alert-padding);
+		width: 100%;
+
+		.icon {
+			width: var(--alert-icon-size);
+			height: var(--alert-icon-size);
+			color: var(--alert-icon-color);
+		}
+	}
+</style>
