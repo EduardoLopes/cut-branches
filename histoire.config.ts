4878ad637f226b5af4409db1972f6f966d1969b6
Eduardo Lopes <eduardolopesds@gmail.com>
2024-11-06 22:13:29 -0300

    refactor: remove histoire configuration file

--- histoire.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'histoire';
-import { HstSvelte } from '@histoire/plugin-svelte';
-
-export default defineConfig({
-	setupFile: 'histoire.setup.ts',
-	plugins: [HstSvelte()],
-	storyIgnored: ['**/node_modules/**', '**/src-tauri/**']
-});
