e491f32ff9264f120048332d4c73c2b557a73d43
Eduardo Lopes <eduardolopesds@gmail.com>
2024-11-21 20:39:31 -0300

    refactor: remove unused histoire type reference

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,5 +1,4 @@
 /// <reference types="@sveltejs/kit" />
-/// <reference types="histoire" />
 
 declare module 'carbon-icons-svelte/*';
 
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { svelteTesting } from '@testing-library/svelte/vite';
 import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
+
 export default defineConfig(({ mode }) => ({
 	plugins: [sveltekit(), svelteTesting()],
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
