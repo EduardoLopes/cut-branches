b9e6a8e9739f430353f7bd6c6fd693d98fe659eb
Eduardo Lopes <eduardolopesds@gmail.com>
2025-03-19 22:19:36 -0300

    chore: add script to link pindova UI

---
+++ scripts/link-pindoba.sh
@@ -0,0 +1,16 @@
+pnpm link ../pindoba/packages/ui/svelte/alert
+pnpm link ../pindoba/packages/ui/svelte/badge
+pnpm link ../pindoba/packages/ui/svelte/button
+pnpm link ../pindoba/packages/ui/svelte/checkbox
+pnpm link ../pindoba/packages/ui/svelte/dialog
+pnpm link ../pindoba/packages/ui/svelte/group
+pnpm link ../pindoba/packages/ui/svelte/loading
+pnpm link ../pindoba/packages/ui/svelte/navigation
+pnpm link ../pindoba/packages/ui/svelte/popover
+pnpm link ../pindoba/packages/ui/svelte/radio
+pnpm link ../pindoba/packages/ui/svelte/select
+pnpm link ../pindoba/packages/ui/svelte/text-input
+
+pnpm link ../pindoba/packages/blocks/svelte/pagination
+pnpm link ../pindoba/packages/blocks/svelte/theme-mode-select
+
